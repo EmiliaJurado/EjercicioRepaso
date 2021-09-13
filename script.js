52ff446b42c8b8a63f6a71d55579c2183fdb1734
EmiliaJurado <emily_9814@hotmail.com>
2021-09-13 15:25:21 -0500

    version 1

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+/*Ejercicio 1 - Desplegar todos los dígitos decimales, del 0 al 9, utilizando una estructura de repetición.
+Tuve que ocupar un ciclo FOR para poder  recorrer la cantidad del 0 hasta el 9, asi mismo utilice console.log 
+para imprimir los digitos ya mencionados. */
+
+/*for (let numero = 0; numero <= 9; numero++){
+    console.log(numero);
+};*/ 
+
+/*Ejercicio 2- Desplegar todos los numeros entre el 50 y 150 con una estructura de repeticion */
+/*for (let numero = 50; numero <= 150; numero++){
+    console.log(numero);
+}; */
+
+/*Ejercicio 3 - Desplegar los números entre el 8 y el 40, saltando de 2 en 2.*/
+
+/*for (let numero = 8; numero <=40; numero++) {
+    console.log(numero++);
+    
+}*/
+
+/*Ejercicio 6 - Desarrollar un programa que con una estructura de repetición imprima la sumatoria de todos los números entre 1 y 50..*/
+
+/*let suma=0;
+for(let num=1; num<=50; num++){
+    console.log(num);
+    suma=suma+num;
+}
+console.log(suma); */
+
+/* Ejercicio 7 - Dado un numero entero positivo obtener la factorial del mismo.*/
+/*let numero=prompt('Ingresa un numero');
+let factorial=1;
+for (let entero= 1; entero<=numero; entero++){
+    factorial=factorial*entero;
+}
+console.log(factorial); 
+*/
+
